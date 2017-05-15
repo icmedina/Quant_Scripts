fa8337f0e673a3ad6f0862699a5006667bb8c66c
IC Medina <icmedina.jr@gmail.com>
2017-05-16 00:13:44 +1200

    Update bloomberg_markets.py

--- bloomberg_markets.py
+++ bloomberg_markets.py
@@ -8,7 +8,7 @@ import json
 # How to get the query URL? Right click on the desired data > Inspect Elements > Select Network Tab > Select XHR (data)
 # Select the source 
 
-###### VARIABLES ######
+###### Global VARIABLES ######
 market = "CUR"	# IND (index), US (stocks)
 period = "1_YEAR" # 1_DAY, 1_MONTH, 1_YEAR, 5_YEAR
 mode = "w"	# outfile mode (w - write; a - append)
@@ -23,7 +23,7 @@ def main():
   # call the functions
   symbols = openSymbols()				# using list of symbols
   #symbols =("AAPL","NFLX")				# using specific symbols
-  getPrice(symbols,market,period)
+  getPrice(symbols)
 
 def openSymbols():
   symbolsFile = open("symbols.txt")
@@ -33,18 +33,18 @@ def openSymbols():
   symbols.pop(len(symbols)-1)				# remove the last \n
   return symbols					# return the list of symbols
 
-def getPrice(symbols,market,period):
+def getPrice(symbols):
   for symbol in symbols:
     url = 'https://www.bloomberg.com/markets/api/bulk-time-series/price/'+symbol+'%3A'+market+'?timeFrame='+period
     #htmlContent = urllib2.request.urlopen(url).read()	# python3.5
     htmlContent = urllib2.urlopen(url).read()  		
     data = json.loads(htmlContent)
-    printPriceDates(symbol,data,mode)
+    printPriceDates(symbol,data)
 
 ## Print Prices and Dates ## 
-def printPriceDates(symbol,data,mode):
+def printPriceDates(symbol,data):
   priceTime = 0
-  outfile = symbol+".csv"
+  outfile = symbol+"-"+period+".csv"
   output = open(outfile, mode)				# create output file
   output.write("Date,Price\n")
 
